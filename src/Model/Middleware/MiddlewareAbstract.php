b67b277e26d0b5f326c91e1d1750ede950c70a15
lukasztecza <lukasztecza.pl@gmail.com>
2018-01-07 20:59:17 -0500

    update app

--- src/Model/Middleware/MiddlewareAbstract.php
+++ src/Model/Middleware/MiddlewareAbstract.php
@@ -1,20 +1,20 @@
 <?php
 namespace TinyApp\Model\Middleware;
 
-use TinyApp\Model\Middleware\ApplicationMiddlewareInterface;
+use TinyApp\Model\Middleware\MiddlewareInterface;
 use TinyApp\Model\System\Request;
 use TinyApp\Model\System\Response;
 
-abstract class ApplicationMiddlewareAbstract implements ApplicationMiddlewareInterface
+abstract class MiddlewareAbstract implements MiddlewareInterface
 {
     private $next;
 
-    public function __construct(ApplicationMiddlewareInterface $next)
+    public function __construct(MiddlewareInterface $next)
     {
         $this->next = $next;
     }
 
-    protected function getNext() : ApplicationMiddlewareInterface
+    protected function getNext() : MiddlewareInterface
     {
         return $this->next;
     }
