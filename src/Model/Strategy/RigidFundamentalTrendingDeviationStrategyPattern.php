201c59226c0d3d2a78334ade14db274cafb4cc2c
lukasztecza <lukasztecza.pl@gmail.com>
2018-04-02 07:21:32 -0400

    modify fundamental strategy

--- src/Model/Strategy/RigidFundamentalTrendingDeviationStrategyPattern.php
+++ src/Model/Strategy/RigidFundamentalTrendingDeviationStrategyPattern.php
@@ -6,13 +6,11 @@ use TinyApp\Model\Service\PriceService;
 use TinyApp\Model\Service\IndicatorService;
 use TinyApp\Model\Strategy\TrendingTrait;
 use TinyApp\Model\Strategy\DeviationTrait;
-use TinyApp\Model\Strategy\FundamentalTrait;
 
 class RigidFundamentalTrendingDeviationStrategyPattern extends RigidStrategyAbstract
 {
     use TrendingTrait;
     use DeviationTrait;
-    use FundamentalTrait;
 
     private $indicatorService;
     private $priceService;
@@ -43,18 +41,32 @@ class RigidFundamentalTrendingDeviationStrategyPattern extends RigidStrategyAbst
 
     protected function getDirection(string $currentDateTime = null, string $selectedInstrument = null) : int
     {
-        $lastIndicators = $this->indicatorService->getLastIndicatorsByPeriod($selectedInstrument, 'P2M', $currentDateTime);
-        $selectedInstrument = $this->selectInstrument($lastIndicators);
-        $fundamental = $this->getFundamental($lastIndicators, $selectedInstrument);
+    //TODO get instruments from injected list
+        $lastIndicators = $this->indicatorService->getLastIndicatorsByPeriod($instruments, 'P3M', $currentDateTime);
+        $indicatorsMap = [];
+        foreach (array_keys($lastIndicators) as $instrument) {
+            $indicatorsMap[$instrument] = 0;
+        }
+
+        $points = ['bank' => 0, 'inflation' => 0, 'companies' => 0, 'trade' => 0, 'unemployment' => 0, 'sales' => 0];
+        foreach ($lastIndicators as $block) {
+             //@TODO loop and see which interest rate is highest and which inflation is highest
+//back loop and set values in pointsMap for all keys if not filled yet for currency and type
+//next loop will over indicatorsMap will assign points and select the best pair to play and now figure out how to find a pair
+//can check for XXX_YYY and YYY_XXX and if one exists then use it and change fundamental value accordingly
+
+
+            var_dump($lastIndicators);exit;
+        }
 
         $lastPrices = $this->priceService->getLastPricesByPeriod($selectedInstrument, 'P7D', $currentDateTime);
         $trend = $this->getTrend($lastPrices, $this->extremumRange);
         $deviation = $this->getDeviation($lastPrices, $this->fastAveragePeriod, $this->slowAveragePeriod);
 
         switch (true) {
-            case $trend === 1 && $deviation === 1 && $fundamental === 1:
+            case $trend === 1 && $deviation === 1:
                 return 1;
-            case $trend === -1 && $deviation === -1 && $fundamental === -1:
+            case $trend === -1 && $deviation === -1:
                 return -1;
             default:
                 return 0;
