79df3a4a395fd9b70013d656c9acdfa5db82f1ac
lukasztecza <lukasztecza.pl@gmail.com>
2018-04-06 20:57:52 -0400

    fix validators calsses

--- src/Model/Validator/ArrayValidatorAbstract.php
+++ src/Model/Validator/ArrayValidatorAbstract.php
@@ -7,56 +7,11 @@ use TinyApp\Model\Validator\ArrayValidatorInterface;
 abstract class ArrayValidatorAbstract implements ValidatorInterface, ArrayValidatorInterface
 {
     protected $error = '';
-    private $csrfToken;
-
-    public function __construct(string $csrfToken)
-    {
-        $this->csrfToken = $csrfToken;
-    }
 
     public function getError() : string
     {
         return $this->error;
     }
 
-    final public function check(Request $request, $checkOrigin = true, $checkCsrfToken = true) : bool
-    {
-        $csrfToken = $request->getPayload(['csrfToken'])['csrfToken'];
-
-        if ($checkOrigin) {
-            switch (true) {
-                case !empty($request->getServer()['HTTP_ORIGIN']):
-                    $httpOrigin = $request->getServer()['HTTP_ORIGIN'];
-                    break;
-                case !empty($request->getServer()['HTTP_REFERER']):
-                    $httpReferer = $request->getServer()['HTTP_REFERER'];
-                    $length = strpos(str_replace('//', '', $httpReferer), '/');
-                    $length = $length > 0 ? $length + 2 : strlen($httpReferer);
-                    $httpOrigin = substr($httpReferer, 0, $length);
-                    break;
-                default:
-                    $httpOrigin = null;
-                    break;
-            }
-
-            if (strpos($request->getHost(), $httpOrigin) !== 0) {
-                $this->error = 'Origin is not valid';
-                return false;
-            }
-        }
-
-        if ($checkCsrfToken && $csrfToken !== $this->csrfToken) {
-            $this->error = 'Csrf token not valid';
-            return false;
-        }
-
-        return $this->validate($request);
-    }
-
-    public function getCsrfToken() : string
-    {
-        return $this->csrfToken;
-    }
-
-    abstract protected function validate(Request $request) : bool;
+    abstract public function check(array $values) : bool
 }
