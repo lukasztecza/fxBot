d8c4814ba559d27cf8e07e0da566ac3e169fe8bd
lukasztecza <lukasztecza.pl@gmail.com>
2018-06-09 13:03:20 -0400

    add stats view

--- src/Model/Repository/TradeRepository.php
+++ src/Model/Repository/TradeRepository.php
@@ -1,6 +1,8 @@
 <?php
 namespace FxBot\Model\Repository;
 
+use LightApp\Model\Repository\RepositoryAbstract;
+
 class TradeRepository extends RepositoryAbstract
 {
     public function saveTrade(array $trade) : int
@@ -20,5 +22,21 @@ class TradeRepository extends RepositoryAbstract
         );
 
         return (int)$affectedId;
+    }
+
+    public function getTrades(string $account, int $page, int $perPage) : array
+    {
+         $trades = $this->getRead()->fetch(
+             'SELECT `id`, `account`, `instrument`, `units`, `price`, `take_profit` takeProfit, `stop_loss` stopLoss, `balance`, `datetime`
+              FROM `trade`
+              WHERE `account` = :account
+              LIMIT ' . ($page - 1) * $perPage . ', ' . $perPage,
+              ['account' => $account]
+         );
+         $pages = $this->getPages(
+            'SELECT COUNT(id) AS count FROM `trade` WHERE `account` = :account', ['account' => $account], $perPage
+         );
+
+         return ['trades' => $trades, 'page' => $page, 'pages' => $pages];
     }
 }
